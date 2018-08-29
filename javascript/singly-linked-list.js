8392a5bd8ec7ff20c4eb606649dc68382ee99be4
snkoski <shawnnkoski@gmail.com>
2018-08-29 15:31:34 -0400

    Rewrite all singly-linked-list.js functions as ES6 class and methods

--- javascript/singly-linked-list.js
+++ javascript/singly-linked-list.js
@@ -3,81 +3,168 @@ function Node(data) {
   this.next = null;
 }
 
-function SinglyLinkedList() {
-  this.head = null;
-  this.tail = null;
-  this.numberOfValues = 0;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.numberOfValues = 0;
+  }
+
+  add(data) {
+    const node = new Node(data);
+    if(!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.numberOfValues++;
+  }
+
+  remove(data) {
+    let previous = this.head;
+    let current = this.head;
+    while(current) {
+      if(current.data === data) {
+        if(current === this.head) {
+          this.head = this.head.next;
+        }
+        if(current === this.tail) {
+          this.tail = previous;
+        }
+        previous.next = current.next;
+        this.numberOfValues--;
+      } else {
+        previous = current;
+      }
+      current = current.next;
+    }
+  }
+
+  insertAfter(data, toNodeData) {
+    let current = this.head;
+    while(current) {
+      if(current.data === toNodeData) {
+        const node = new Node(data);
+        if(current === this.tail) {
+          this.tail.next = node;
+          this.tail = node;
+        } else {
+          node.next = current.next
+          current.next = node;
+        }
+        this.numberOfValues++;
+      }
+      current = current.next;
+    }
+  }
+
+  traverse(fn) {
+    let current = this.head;
+    while(current) {
+      if(fn) {
+        fn(current);
+      }
+      current = current.next;
+    }
+  }
+
+  length() {
+    return this.numberOfValues;
+  }
+
+  print() {
+    let string = '';
+    let current = this.head;
+    while(current) {
+      string += current.data + ' ';
+      current = current.next;
+    }
+    console.log(string.trim());
+  }
 }
 
-SinglyLinkedList.prototype.add = function(data) {
-  var node = new Node(data);
-  if(!this.head) {
-    this.head = node;
-    this.tail = node;
-  } else {
-    this.tail.next = node;
-    this.tail = node;
-  }
-  this.numberOfValues++
-};
-SinglyLinkedList.prototype.remove = function(data) {
-  var previous = this.head;
-  var current = this.head;
-  while(current) {
-    if(current.data === data) {
-      if(current === this.head) {
-        this.head = this.head.next;
-      }
-      if(current === this.tail) {
-        this.tail = previous;
-      }
-      previous.next = current.next;
-      this.numberOfValues--;
-    } else {
-      previous = current;
-    }
-    current = current.next;
-  }
-};
-SinglyLinkedList.prototype.insertAfter = function(data, toNodeData) {
-  var current = this.head;
-  while(current) {
-    if(current.data === toNodeData) {
-      var node = new Node(data);
-      if(current === this.tail) {
-        this.tail.next = node;
-        this.tail = node;
-      } else {
-        node.next = current.next;
-        current.next = node;
-      }
-      this.numberOfValues++;
-    }
-    current = current.next;
-  }
-};
-// Traverse allows you to perform actions on all elements of the list
-SinglyLinkedList.prototype.traverse = function(fn) {
-  var current = this.head;
-  while(current) {
-    if(fn) {
-      fn(current);
-    }
-    current = current.next;
-  }
-};
-SinglyLinkedList.prototype.length = function() {
-  return this.numberOfValues;
-};
-SinglyLinkedList.prototype.print = function() {
-  var string = '';
-  var current = this.head;
-  while(current) {
-    string += current.data + ' ';
-    current = current.next;
-  }
-  console.log(string.trim());
-};
+// function Node(data) {
+//   this.data = data;
+//   this.next = null;
+// }
+//
+// function SinglyLinkedList() {
+//   this.head = null;
+//   this.tail = null;
+//   this.numberOfValues = 0;
+// }
+//
+// SinglyLinkedList.prototype.add = function(data) {
+//   var node = new Node(data);
+//   if(!this.head) {
+//     this.head = node;
+//     this.tail = node;
+//   } else {
+//     this.tail.next = node;
+//     this.tail = node;
+//   }
+//   this.numberOfValues++
+// };
+// SinglyLinkedList.prototype.remove = function(data) {
+//   var previous = this.head;
+//   var current = this.head;
+//   while(current) {
+//     if(current.data === data) {
+//       if(current === this.head) {
+//         this.head = this.head.next;
+//       }
+//       if(current === this.tail) {
+//         this.tail = previous;
+//       }
+//       previous.next = current.next;
+//       this.numberOfValues--;
+//     } else {
+//       previous = current;
+//     }
+//     current = current.next;
+//   }
+// };
+// SinglyLinkedList.prototype.insertAfter = function(data, toNodeData) {
+//   var current = this.head;
+//   while(current) {
+//     if(current.data === toNodeData) {
+//       var node = new Node(data);
+//       if(current === this.tail) {
+//         this.tail.next = node;
+//         this.tail = node;
+//       } else {
+//         node.next = current.next;
+//         current.next = node;
+//       }
+//       this.numberOfValues++;
+//     }
+//     current = current.next;
+//   }
+// };
+// // Traverse allows you to perform actions on all elements of the list
+// SinglyLinkedList.prototype.traverse = function(fn) {
+//   var current = this.head;
+//   while(current) {
+//     if(fn) {
+//       fn(current);
+//     }
+//     current = current.next;
+//   }
+// };
+// SinglyLinkedList.prototype.length = function() {
+//   return this.numberOfValues;
+// };
+// SinglyLinkedList.prototype.print = function() {
+//   var string = '';
+//   var current = this.head;
+//   while(current) {
+//     string += current.data + ' ';
+//     current = current.next;
+//   }
+//   console.log(string.trim());
+// };
 
 var singlyLinkedList = new SinglyLinkedList();
 singlyLinkedList.print();
